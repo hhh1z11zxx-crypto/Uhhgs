fabdbf0d08fd11b5aa5da49d183bdaa95baa0125
hhh1z11zxx-crypto <hhh1z11zxx@gmail.com>
2025-10-08 22:32:51 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,14 +1,10 @@
 {
-  "name": "taixiu-predictor",
-  "version": "1.0.0",
-  "description": "Tai Xiu prediction server using Fastify and WebSockets",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js"
-  },
   "dependencies": {
     "@fastify/cors": "^8.0.0",
     "fastify": "^4.0.0",
     "ws": "^8.0.0"
+  },
+  "scripts": {
+    "start": "node server.js" 
   }
 }
